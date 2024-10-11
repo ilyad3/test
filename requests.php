a3c18ceaa94d84acd9d6fb7fec0d3189e7e3b339
Ilya <ilyad3@ya.ru>
2024-10-12 00:00:07 +0300

    Update requests.php

--- requests.php
+++ requests.php
@@ -2,6 +2,6 @@
 if ($_SERVER['REQUEST_METHOD'] == "GET") {
     echo "Метод GET<br>Ваше Имя: ".$_REQUEST['name'];
 }elseif ($_SERVER['REQUEST_METHOD'] == "POST") {
-    echo "Метод POST<br>Ваш Возраст: ".$_REQUEST['age'];
+    echo "Метод POST\nВаш Возраст: ".$_REQUEST['age'];
 }
 ?>
