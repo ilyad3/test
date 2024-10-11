00a6bc219cf7818ca614404482194759b739240b
Ilya <ilyad3@ya.ru>
2024-10-11 11:13:23 +0300

    Update test.php

--- test.php
+++ test.php
@@ -1,4 +1,11 @@
 <?php
-echo '124124';
 
+$name = $_GET['name'];
+$second_name = $_GET['second_name'];
+$group = $_GET['group'];
+$birthday = $_GET['birthday'];
+$git = $_GET['git'];
+
+
+echo "Ваше имя: ".$name."\nВаша фамилия: ".$second_name."\nВаша группа: ".$group."\nВаша дата рождения: ".$birthday."\nСсылка на ваш гит: <a href='".$git."' target='_blank'>".$git."</a>";
 ?>
